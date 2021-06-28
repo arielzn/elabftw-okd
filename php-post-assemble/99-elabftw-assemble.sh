097c62f4c61513eeb03f5b4940420decf7f8bd48
arielzn <arielzn@gmail.com>
2021-06-28 16:32:38 +0200

    bump version to 4.0.3

--- php-post-assemble/99-elabftw-assemble.sh
+++ php-post-assemble/99-elabftw-assemble.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# cleanup pre-assebemble artifacts
+rm -rvf ./php-pre-assemble
+
 # after build the nodejs modules used by the app has been copied inside web/
 rm -rvf ./node_modules
-
